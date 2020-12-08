81c5eff5b10a2b84aa25017b86504cd7c9af670e
Daniel Kopecký <kopecky.d@gmail.com>
2020-12-08 09:59:35 +0100

    Changed constants names

--- nr_events/record.py
+++ nr_events/record.py
@@ -3,7 +3,7 @@ from invenio_records.api import Record
 from oarepo_references.mixins import ReferenceEnabledRecordMixin
 from oarepo_validate import SchemaKeepingRecordMixin, MarshmallowValidatedRecordMixin
 
-from .constants import THESES_ALLOWED_SCHEMAS, THESES_PREFERRED_SCHEMA
+from .constants import EVENTS_ALLOWED_SCHEMAS, EVENTS_PREFERRED_SCHEMA
 from .marshmallow import EventsMetadataSchemaV1
 
 
@@ -11,8 +11,8 @@ class PublishedEventRecord(SchemaKeepingRecordMixin,
                            MarshmallowValidatedRecordMixin,
                            ReferenceEnabledRecordMixin,
                            Record):
-    ALLOWED_SCHEMAS = THESES_ALLOWED_SCHEMAS
-    PREFERRED_SCHEMA = THESES_PREFERRED_SCHEMA
+    ALLOWED_SCHEMAS = EVENTS_ALLOWED_SCHEMAS
+    PREFERRED_SCHEMA = EVENTS_PREFERRED_SCHEMA
     MARSHMALLOW_SCHEMA = EventsMetadataSchemaV1
 
     @property
