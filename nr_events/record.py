e09dbce35eb0fc493f5703161fe0c397783d5e4e
Daniel Kopecký <kopecky.d@gmail.com>
2021-03-11 14:11:27 +0100

    Fixed date and removed CanonicalUrlMixin

--- nr_events/record.py
+++ nr_events/record.py
@@ -1,5 +1,5 @@
+from flask import url_for
 from invenio_records.api import Record
-from nr_common.record import CanonicalUrlMixin
 from oarepo_references.mixins import ReferenceEnabledRecordMixin
 from oarepo_validate import SchemaKeepingRecordMixin, MarshmallowValidatedRecordMixin
 
@@ -10,7 +10,6 @@ from .marshmallow import EventsMetadataSchemaV1
 class PublishedEventRecord(SchemaKeepingRecordMixin,
                            MarshmallowValidatedRecordMixin,
                            ReferenceEnabledRecordMixin,
-                           CanonicalUrlMixin,
                            Record):
     ALLOWED_SCHEMAS = EVENTS_ALLOWED_SCHEMAS
     PREFERRED_SCHEMA = EVENTS_PREFERRED_SCHEMA
@@ -18,5 +17,5 @@ class PublishedEventRecord(SchemaKeepingRecordMixin,
 
     @property
     def canonical_url(self):
-        return self.get_canonical_url('invenio_records_rest.events_item',
+        return url_for('invenio_records_rest.events_item',
                                       pid_value=self['control_number'], _external=True)
