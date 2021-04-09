44431d73c7a967fc6acbaff49d4ccf1ccf45176d
Mirek Simek <miroslav.simek@gmail.com>
2021-04-09 12:02:28 +0200

    canonical url for all records

--- nr_events/record.py
+++ nr_events/record.py
@@ -9,7 +9,8 @@ from oarepo_records_draft.record import InvalidRecordAllowedMixin, DraftRecordMi
 from oarepo_references.mixins import ReferenceEnabledRecordMixin
 from oarepo_validate import SchemaKeepingRecordMixin, MarshmallowValidatedRecordMixin
 
-from .constants import EVENTS_ALLOWED_SCHEMAS, EVENTS_PREFERRED_SCHEMA, published_index_name, draft_index_name, all_events_index_name
+from .constants import EVENTS_ALLOWED_SCHEMAS, EVENTS_PREFERRED_SCHEMA, published_index_name, draft_index_name, \
+    all_events_index_name
 from .marshmallow import EventsMetadataSchemaV1
 
 
@@ -52,4 +53,16 @@ class AllEventRecord(SchemaKeepingRecordMixin, CommunityRecordMixin, Record):
     ALLOWED_SCHEMAS = EVENTS_ALLOWED_SCHEMAS
     PREFERRED_SCHEMA = EVENTS_PREFERRED_SCHEMA
     index_name = all_events_index_name
-    # TODO: better canonical url based on if the class is published or not
+
+    @property
+    def canonical_url(self):
+        if not self.get('oarepo:draft'):
+            endpoint = 'invenio_records_rest.events-community_item'
+        else:
+            endpoint = 'invenio_records_rest.draft-events-community_item'
+
+        return url_for(endpoint,
+                       pid_value=CommunityPIDValue(
+                           self['control_number'],
+                           current_oarepo_communities.get_primary_community_field(self)),
+                       _external=True)
